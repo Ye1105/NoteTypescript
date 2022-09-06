92c62e0ab329670bc78b617b05ddbe6289dbe527
15868707168@163.com <15868707168@163.com>
2022-09-06 12:34:48 +0800

    更新

--- 01_hello.js
+++ 01_hello.js
@@ -1,15 +1,9 @@
-<!--
+/*
  * @Author: 15868707168@163.com 15868707168@163.com
- * @Date: 2022-09-06 11:22:17
+ * @Date: 2022-09-06 11:53:52
  * @LastEditors: 15868707168@163.com 15868707168@163.com
- * @LastEditTime: 2022-09-06 12:05:22
- * @FilePath: \TS_PROJECT\README.md
+ * @LastEditTime: 2022-09-06 11:54:38
+ * @FilePath: \TS_PROJECT\1.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-# TS_PROJECT
-
-1.node.js
-
-2.npm i -g typescrpit
-
-3.tsc **.ts
+ */
+console.log("hello ts");
