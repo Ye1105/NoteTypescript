4efb25b932f3a49c09da6a5d0365cb3bdb1410c4
15868707168@163.com <15868707168@163.com>
2022-09-13 17:53:37 +0800

    更新

--- TS/25_代理模式.ts
+++ TS/25_代理模式.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 15868707168@163.com 15868707168@163.com
+ * @Date: 2022-09-13 12:05:38
+ * @LastEditors: 15868707168@163.com 15868707168@163.com
+ * @LastEditTime: 2022-09-13 14:47:38
+ * @FilePath: \TS_PROJECT\TS\25_代理模式.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 
 interface Calulate{
@@ -20,9 +28,8 @@ class NPC2 implements Calulate{
 
 
 class Person{    
+    
     delegate:Calulate
-
-    constructor(){}
 
     getNum(num1:number,num2:number){
         /* 拿到num1和num2计算后的结果 */
